c2c0dffe1620812e6919ab36e3115857c33ccdea
SiaESin <siabhon.sindab@gmail.com>
2023-09-10 16:18:11 -0700

    jwt branch addition

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -3,20 +3,24 @@ import { useEffect } from "react";
 
 export const CurrentUser = createContext()
 
-function CurrentUserProvider({ children }){
+function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
     useEffect(() => {
 
         const getLoggedInUser = async () => {
             let response = await fetch('http://localhost:5000/authentication/profile', {
-                credentials: 'include'
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
             })
             let user = await response.json()
             setCurrentUser(user)
         }
         getLoggedInUser()
     }, [])
+  
+
 
 
     return (
